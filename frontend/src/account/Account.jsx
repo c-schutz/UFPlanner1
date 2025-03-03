ebe906f5ffda3ac6fea598d0aa56be2def151ad9
PhilStef <philstefanowicz@aol.com>
2025-03-03 17:44:56 -0500

    Started working on the signup and log in page in the account page

--- frontend/src/account/Account.jsx
+++ frontend/src/account/Account.jsx
@@ -1,11 +1,26 @@
-function Account() {
-    return (
-        <>
-        <p>
-            Account
-        </p>
-        </>
+import { Link, Routes, Route } from 'react-router-dom';
+import Login from "./Login"
+import Signup from "./Signup"
+const Account = () => {
+    return ( 
+      <div>
+        <h2>Account Page</h2>
+        <nav>
+          <ul>
+            <li>
+              <Link to="login">Login</Link>
+            </li>
+            <li>
+              <Link to="signup">Signup</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route to='Account/Login' element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Routes>
+      </div>
     );
-}
+  };
 
 export default Account;
