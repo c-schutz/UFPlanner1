1cb285ad82f45de8c9869a113880893a0a32c689
c-schutz <cschutz@ufl.edu>
2025-03-04 12:09:14 -0800

    Added button animation w framer motion

--- frontend/src/budget/Budget.jsx
+++ frontend/src/budget/Budget.jsx
@@ -1,15 +1,41 @@
-import "./bstyles.css";
+import React, { useState, useEffect } from 'react';
+import { motion, useAnimate } from "motion/react"
+
+import './bstyles.css';
 
 function Budget() {
+    const [scope, animate] = useAnimate()
+    const [delay, setDelay] = useState(false);
+
+    async function myAnimation() {
+        await animate("button", { y:-5 }, {duration: .3});
+        await animate("button", { y: 5 }, {duration: .3});
+      }
+
+    const handleClick = () => {
+        if (!delay) {
+            setDelay(true); //debounce
+            console.log("test");
+            myAnimation();
+            const tout = setTimeout(() =>{ //setTimeout only runs once vs setInterval which runs multiple times
+                setDelay(false);    
+            }, 1000);
+            return() => clearTimeout(tout);
+        }
+    }
+
+
     return (
         <>
-            <title> UFPlanner Budget </title>
-            <p className="btitle"> Here you can create your own budgets! </p>
-                <hr className="bhr"></hr>
-                <button className="new-budget-button">
+            <p className="btitle">Here you can create your own budgets!</p>
+            <hr className="bhr" />
+            <div ref={scope}>
+                <motion.button className='new-budget-button' onClick={handleClick}
+                whileHover={{scale: 1.1}}>
                     <span>+</span>
                     New Budget
-                </button>
+                </motion.button>
+            </div>
         </>
     );
 }
