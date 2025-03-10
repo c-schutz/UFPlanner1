16c954eb1642a5ec16a7dddd3e4a0c84ec1e00a1
PhilStef <philstefanowicz@aol.com>
2025-03-10 18:49:23 -0400

    Setting up inputs

--- frontend/src/account/Login.jsx
+++ frontend/src/account/Login.jsx
@@ -3,14 +3,37 @@ import {React, useState} from "react";
 const Login = ({ onStatusChange }) => {
     const[email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const handeLogin = (e) => {
-        e.preventDefault();
-    }
+    function checkInputs(){
+        console.log("Email: " , email);
+        console.log("password: " , password);
 
+    }
+      
 
     return (
-        <>
-        <div>Login Feature</div>
+        <> 
+        <div>Login Feature</div> 
+        <div> 
+        <input
+          value={email}
+          onChange={e => {
+            setEmail(e.target.value) 
+            checkInputs()
+            }
+          }
+        />
+        </div>
+        <div>
+        <input
+          value={password}
+          onChange={e => {
+            setPassword(e.target.value)
+            checkInputs()
+            }
+        }
+
+        />
+        </div>
         <div><button onClick={() => onStatusChange('summary')}>Login</button></div>
         <div><button onClick={() => onStatusChange('signup')}>Sign Up</button></div>
         </>
