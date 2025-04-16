03797e831991d05f36c825c1e59bc72ae543f41c
PhilStef <philstefanowicz@aol.com>
2025-04-16 16:28:51 -0400

    added first and last names

--- frontend/src/account/Summary.jsx
+++ frontend/src/account/Summary.jsx
@@ -5,15 +5,18 @@ import './Account.css';
 const Summary = ({ handleStatus }) => {
     const { logged, setLogged } = useVData();
     const [email, setEmail] = useState("");
-    const [dateCreated, setDateCreated] = useState("");
+    const [dateCreated, setDateCreated] = useState(""); 
+    const [fName, setName] = useState("");
 
     useEffect(() => {
     const storedEmail = sessionStorage.getItem("email");
     const storedDate = sessionStorage.getItem("date_created");
-
-    if (storedEmail && storedDate) {
+    const name = sessionStorage.getItem('name'); 
+    console.log(name)
+    if (storedEmail && storedDate && name) {
       setEmail(storedEmail.slice(1,-1));
       setDateCreated(storedDate.slice(1, 10));
+      setName(name.slice(1,-1));
     }
   }, []);
     const signout = () => {
@@ -24,6 +27,7 @@ const Summary = ({ handleStatus }) => {
     return (
         <>
       <div className="account-title">Account Summary</div>
+      <div><strong>Hello,</strong> {fName}!</div>
       <div><strong>Email:</strong> {email}</div>
       <div><strong>Date Created:</strong> {dateCreated}</div>
       <div><button onClick={signout}>Signout</button></div>
